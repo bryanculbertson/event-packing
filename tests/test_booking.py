c5032caf814a28031f10d1e35f4e0d177856a90a
Bryan Culbertson <bryan.culbertson@gmail.com>
2021-10-20 23:56:27 +0000

    Verify algorithm

--- tests/test_booking.py
+++ tests/test_booking.py
@@ -8,6 +8,14 @@ def test_book_meetings(
 ) -> None:
     for test_name, (meetings, num_attendees) in example_meetings.items():
         booked_meetings, booked_attendees = book_meetings(meetings)
+
+        all_attendees = []
+        for meeting in booked_meetings:
+            for attendee in meeting:
+                all_attendees.append(attendee)
+
+        assert len(set(all_attendees)) == len(all_attendees)
+        assert len(all_attendees) == len(booked_attendees)
         assert (
             len(booked_attendees) == num_attendees
         ), f"Failed {test_name}: {booked_meetings} with {booked_attendees}"
