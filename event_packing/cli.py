b2da629ab09488ae520f969a5e2211e19bdec465
Bryan Culbertson <bryan.culbertson@gmail.com>
2021-10-20 23:25:01 +0000

    Initial single pass algorithm

--- event_packing/cli.py
+++ event_packing/cli.py
@@ -7,8 +7,12 @@ Example usage:
     poetry run event-packing version
 
 """
+import json
+
 import click
 import dotenv
+
+from event_packing.booking import book_meetings
 
 
 @click.group()
@@ -21,6 +25,23 @@ def cli() -> None:
 def version() -> None:
     """Get the cli version."""
     click.echo(click.style("0.1.0", bold=True))
+
+
+@cli.command()
+@click.argument("meetings_spec", type=str, required=True)
+def book(meetings_spec: str) -> None:
+    """Book non-conflicting meetings with maximimum total attendees"""
+    meetings_list = json.loads(meetings_spec)
+
+    if not isinstance(meetings_list, list):
+        raise Exception("Meetings spec must be a list of lists")
+
+    if not isinstance(meetings_list[0], list):
+        raise Exception("Meetings spec must be a list of lists")
+
+    meetings, attendees = book_meetings(meetings_list)
+    meetings_result = json.dumps(meetings, separators=(",", ":"))
+    print(f"`{meetings_result}` which has `{len(attendees)}` attendees")
 
 
 if __name__ == "__main__":
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,6 @@
 from click.testing import CliRunner
 
-from event_packing.cli import version
+from event_packing.cli import book, version
 
 
 def test_version() -> None:
@@ -8,3 +8,10 @@ def test_version() -> None:
     result = runner.invoke(version)
     assert result.exit_code == 0
     assert result.output == "0.1.0\n"
+
+
+def test_book() -> None:
+    runner = CliRunner()
+    result = runner.invoke(book, "[[0,1,2]]")
+    assert result.exit_code == 0
+    assert result.output == "`[[0,1,2]]` which has `3` attendees\n"
